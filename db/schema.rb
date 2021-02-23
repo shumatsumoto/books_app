1f0d199eeb3bad44806866e831b415735849e83b
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-02-23 22:16:44 +0900

    画像投稿機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_124607) do
+ActiveRecord::Schema.define(version: 2021_02_23_130600) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_124607) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
